073ee6682eb9d560ea41e9618c4494ab0cee97d9
Saurabh Kesharwani <saurabh.k@sudoboat.com>
2022-09-05 14:50:52 +0530

    My first comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react' ;
+import Resturant from './Components/Basic/Resturant' ;
+
+const App = () => {
+    return (
+        <div>
+           <Resturant /> 
+        </div>
+    )
+}
+
+export default App
+
